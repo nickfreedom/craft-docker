8706167d18f68c1c75c7b6da06d4d715a628d50c
Nick Le Guillou <nick.leguillou@gmail.com>
2018-07-08 11:54:54 -0500

    Added API routes for devices and detail

---
+++ config/element-api.php
@@ -0,0 +1,39 @@
+<?php
+
+use craft\elements\Entry;
+use craft\helpers\UrlHelper;
+
+return [
+    'endpoints' => [
+        'devices' => [
+            'elementType' => Entry::class,
+            'criteria' => ['section' => 'devices'],
+            'transformer' => function(Entry $entry) {
+                return [
+                    'title' => $entry->title,
+                    'detailUrl' => UrlHelper::url("devices/{$entry->id}"),
+                    'serialNumber' => $entry->getFieldValue('serialNumber'),
+                    'key' => $entry->getFieldValue('key'),
+                    'lastUpdate' => $entry->dateUpdated->format('Y-m-d H:i:s')
+                ];
+            },
+        ],
+        'devices/<entryId:\d+>' => function($entryId) {
+            return [
+                'elementType' => Entry::class,
+                'criteria' => ['id' => $entryId],
+                'one' => true,
+                'transformer' => function(Entry $entry) {
+                    return [
+                        'title' => $entry->title,
+                        'lastUpdate' => $entry->dateUpdated->format('Y-m-d H:i:s'),
+                        'serialNumber' => $entry->getFieldValue('serialNumber'),
+                        'key' => $entry->getFieldValue('key'),
+                        'lastRecording' => $entry->getFieldValue('lastRecording')
+                    ];
+                },
+            ];
+        },
+
+    ]
+];
